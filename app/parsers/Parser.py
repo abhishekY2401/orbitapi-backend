c0f3ff11721ba541f71779a84557e0d9a0bee0f4
abhishekY2401 <abhiyd2401@gmail.com>
2024-11-09 22:31:20 +0530

    universal parser class to extract specs based on the framework type

--- app/parsers/Parser.py
+++ app/parsers/Parser.py
@@ -1,25 +1,21 @@
-from app.services.parser import extract_api_from_nodejs, generate_api_specs_for_django
-from app.schema.api_schema import ApiSpecsResponseModel
+from typing import List, Dict, Any
+from app.schema.api_schema import APISpecification
+from app.parsers.NodeParser import NodeJSParser
+from pathlib import Path
 
 
 class Parser:
+    def __init__(self, repo_path: str, framework_type: str, routes_path: str):
+        self.repo_path = repo_path
+        self.framework_type = framework_type.lower()
+        self.routes_path = Path(routes_path)
 
-    def __init__(self, repo_url: str, framework_type: str) -> None:
-        self.repo_url = repo_url
-        self.framework_type = framework_type
+    def parse(self) -> List[Dict[str, Any]]:
+        """
+        Parse the repository and extract API specifications
+        """
 
-    def parse(self):
-        # invoke framework parser types
-        if self.framework_type.lower() == 'nodejs':
-            return self.parse_nodejs()
-        elif self.framework_type.lower() == 'django':
-            return self.parse_django()
-        else:
-            raise ValueError(
-                "Unsupported framework type. Use 'nodejs' or 'django'.")
-
-    def parse_nodejs(self) -> ApiSpecsResponseModel:
-        return extract_api_from_nodejs(self.repo_url)
-
-    def parse_django(self) -> ApiSpecsResponseModel:
-        return generate_api_specs_for_django(self.repo_url)
+        parser = NodeJSParser(
+            routes_path=str(self.routes_path), repo_path=self.repo_path
+        )
+        return parser.extract_apis()
